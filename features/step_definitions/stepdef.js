3a16bf9502c87548f47db2486e37eefb35f86300
PrzemekStasiak <140719984+PrzemekStasiak@users.noreply.github.com>
2024-05-24 21:41:50 +0200

    homework done

---
+++ features/step_definitions/stepdef.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+
+
+Given('using a search field find alkaline batteries', function () {
+// Write code here that turns the phrase above into concrete actions
+return 'pending';
+});
+
+
+
+When('ad batteries to shoping cart', function () {
+// Write code here that turns the phrase above into concrete actions
+return 'pending';
+});
+
+
+
+Then('buying batteries with buy button', function () {
+// Write code here that turns the phrase above into concrete actions
+return 'pending';
+});
---
+++ features/store_scenario.feature
@@ -0,0 +1,9 @@
+Feature: Logged user buying batteries on allegro.pl
+
+Scenario: using a search field find alkaline AA batteries
+
+Given using a search field find alkaline batteries
+
+When ad batteries to shoping cart
+
+Then buying batteries with buy button
